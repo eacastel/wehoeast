f962c84a3e5a38e0b4535e9cb3b6651792919f2d
eacastel <emilio@aliigo.com>
2020-12-08 17:10:21 -0800

    more images

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import LogoIcon from '../../svg/LogoIcon';
 import Button from '../Button';
-import LogoImg from  "../../svg/wehoeastside-logo-650.svg";
+import LogoImg from  "../../images/wehoeastside-logo-650.gif";
 
 const Header = () => (
   <header className="sticky top-0 bg-white shadow">
