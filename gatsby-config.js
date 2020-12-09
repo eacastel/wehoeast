0be4b4804b62cde93c88477f65f00ed166ffcfc2
eacastel <emilio@aliigo.com>
2020-12-09 14:18:37 -0800

    styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
