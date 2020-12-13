0b28fc1414adbec4058f89201b157f347e729887
eacastel <emilio@aliigo.com>
2020-12-12 20:44:03 -0800

    404-c-husky

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,31 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from '../components/layout/Layout';
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
 const NotFound = () => {
+
+    const data = useStaticQuery(graphql`
+    query {
+      img404: file(relativePath: {eq: "404.png"}) {
+        childImageSharp {
+          fluid(maxWidth:800) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
-      <h1>Page not found</h1>
-      <p>
-        <Link to="/">Head home</Link>
-      </p>
+        <div className="justify-center px-8 mt-20 text-center">
+      <h1 className="text-3xl font-medium">404 / Page not found</h1>
+      <Link to="/" className="text-lg font-medium text-tmagenta hover:text-tyellow">Head home</Link>
+      <Link to="/"><Img fluid={data.img404.childImageSharp.fluid} alt="404 cute image of kitty and husky kissing" className="object-center mx-8" /></Link>
+
+        </div>
     </Layout>
   )
 }
