1a066f49a833698a9563d0a18861b1b0ac34c77f
eacastel <emilio@aliigo.com>
2020-12-16 10:22:07 -0800

    text-weho east

--- src/components/FeatureOne.js
+++ src/components/FeatureOne.js
@@ -21,7 +21,7 @@ const FeatureOne = () => {
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-            Join the WEHO Eastside Neighborhood Watch Group Today!
+            Join WEHO EAST Neighborhood Watch Group Today!
           </h1>
           <p className="text-xl mb-8 lg:text-2xl mt-6 font-light">
             Connect with your neighborhood Capitans, get important information, and make your voice
